8682d2ac3fe0ca93caa0c73a6da0ce330e87ef5f
njmyers <17010801+njmyers@users.noreply.github.com>
2018-01-30 03:21:30 -0500

    react filepaths

--- deploy/enforce-website.js
+++ deploy/enforce-website.js
@@ -1,15 +1,19 @@
 const chalk = require('chalk');
 const { putBucketWebsite, getBucketWebsite } = require('../lib/s3');
 
-const params = {
-	IndexDocument: { Suffix: 'index.html' },
-	ErrorDocument: { Key: 'index.html' },
-	RoutingRules: [] }
-}
-
 async function enforceWebsite({ Bucket } = {}) {
+	const WebsiteConfiguration = {
+		IndexDocument: { 
+			Suffix: 'index.html'
+		},
+		ErrorDocument: {
+			Key: 'index.html'
+		},
+		RoutingRules: []
+	}
+
 	try {
-		const response = await getBucketWebsite({ Bucket });
+		const response = await putBucketWebsite({ Bucket, WebsiteConfiguration });
 		console.log(response);
 		// return response
 	} catch(e) {
