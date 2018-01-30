1e0d065a97e945612912dde7d8d73bc4aa02281c
njmyers <17010801+njmyers@users.noreply.github.com>
2018-01-30 13:09:47 -0500

    testing website policy

--- deploy/enforce-website.js
+++ deploy/enforce-website.js
@@ -1,16 +1,8 @@
 const chalk = require('chalk');
 const { putBucketWebsite, getBucketWebsite } = require('../lib/s3');
+const config = '{\"IndexDocument\":{\"Suffix\":\"index.html\"},\"ErrorDocument\":{\"Key\": \"index.html\"},\"RoutingRules\": []}';
 
-async function enforceWebsite({ Bucket } = {}) {
-	const WebsiteConfiguration = {
-		IndexDocument: { 
-			Suffix: 'index.html'
-		},
-		ErrorDocument: {
-			Key: 'index.html'
-		},
-		RoutingRules: []
-	}
+async function enforceWebsite({ Bucket, WebsiteConfiguration = config } = {}) {
 
 	try {
 		const response = await putBucketWebsite({ Bucket, WebsiteConfiguration });
