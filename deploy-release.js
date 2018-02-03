bd1112384f8c53f24f0a627a8163a22205d0f37d
njmyers <17010801+njmyers@users.noreply.github.com>
2018-02-03 08:11:21 -0500

    added task runner

--- deploy-release.js
+++ deploy-release.js
@@ -1,102 +1,51 @@
 #!/usr/bin/env node
 
 const chalk = require('chalk');
-const { putDirectoryToS3, putDeployLog, getDirectoryFromS3, deleteObjects } = require('./lib/s3');
-const { mergeDeployLog } = require('./lib/log');
-const { readDirectory, stripAllExceptKey } = require('./lib/dir');
-const { validateRelease, validateCredentials, logTask, consoleOutput, enforcePolicy, enforceWebsite } = require('./deploy');
+const { run } = require('./lib/run');
 
-function validateResolutions(resolutions) {
-	return resolutions;
-}
+const { validateRelease,
+		enforceBucketPolicy,
+		enforceWebsitePolicy,
+		readOldDeploy,
+		archiveOldDeploy,
+		readBuildDirectory,
+		uploadCurrentBuild,
+		uploadNewLog } = require('./lib/tasks');
 
 async function deploy() {
 
-	console.log(chalk.yellow('deploying your build to S3...'));
-
 	try {
-		
-		logTask('validating release', 'started');
-		const { Bucket, log, release, cwd } = await validateRelease();
-		logTask('validating release', 'completed');
-		await enforceWebsite({ Bucket });
 
-		logTask('reading old deploy', 'started');
-		const oldContainers = await getDirectoryFromS3({
-			Bucket,
-			Prefix: 'current'
-		});
-		logTask('reading old deploy', 'completed');
+		// Get bucket name, remote log, current local directory and generate new release
+		const { Bucket, log, release, cwd } = await run(validateRelease);
 
-		// console.log(oldContainers);
-		if (oldContainers.length > 0) {
+		// download current build as readable streams		
+		const current = await run(readOldDeploy, Bucket);
 
-			// put last deploy streams to S3 in new directory
-			logTask('archiving old deploy', 'started');
-			const resolve = await putDirectoryToS3({
-				Bucket,
-				containers: oldContainers,
-				dest: `releases/${log.current.id}`,
-				stub: 'current'
-			});
-			logTask('archiving old deploy', 'completed');
+		// if current build archive under log id number
+		const archived = current.length > 0 ? await run(archiveOldDeploy, Bucket, current, log.current.id) : true;
 
-			logTask('deleting old deploy', 'started');
-			const deleted = await deleteObjects({
-				Bucket,
-				Delete: {
-					Objects: stripAllExceptKey(oldContainers)
-				}
-			});
-			logTask('deleting old deploy', 'completed');
-		}
-
-		// read build directory recursively & synchronously then creates readable streams
-		logTask('reading build directory', 'started');
-		const containers = readDirectory(`${cwd}/build`);
-		logTask('reading build directory', 'completed');
+		// read build directory (sync) recursively & then create readable streams & headers
+		const containers = run(readBuildDirectory, cwd);
 
 		// put streams to S3 and rename to current
-		logTask('uploading current build', 'started');
-		const resolutions = await putDirectoryToS3({
-			Bucket,
-			containers,
-			dest: 'current',
-			stub: 'build'
-		});
-		validateResolutions(resolutions);
-		logTask('uploading current build', 'completed');
+		await run(uploadCurrentBuild, Bucket, containers);
 
+		// update log
+		await run(uploadNewLog, Bucket, log, release);
 
-		logTask('merging and uploading new log', 'started');
-		const newLog = mergeDeployLog(log, release);
-		const resolution = await putDeployLog({
-			Bucket,
-			Body: JSON.stringify(newLog, null, 4)
-		});
-		logTask('merging and uploading new log', 'completed');
+		// enforce bucket policy
+		await run(enforceBucketPolicy, Bucket);
 
-		validateResolutions(resolution);
-
-		logTask('enforcing policy', 'started');
-		const policy = await enforcePolicy({ Bucket })
-		logTask('enforcing policy', 'completed');
+		// enforce website policy on bucket
+		await run(enforceWebsitePolicy, Bucket);
 		
-		consoleOutput(release);
-
+		// ouptut release info
+		run(logNewRelease, release);
+		
 	} catch(e) {
 		console.log(chalk.red(e));		
 	}
 }
 
 deploy();
-
-async function taskManager(cb) {
-
-}
-
-/*
-	log task
-	run function
-
- */
--- deploy/__test__/validate-bucket.test.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config({});
-const validateBucket = require('../validate-bucket');
-
-const Bucket = process.env.S3_DEPLOY_BUCKET;
-
-test('returns bucket correctly', () => {
-	expect(validateBucket()).toBe(Bucket);
-});
